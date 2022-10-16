2e34336f88d92d77d9569cd40d318a646479dc95
Cleiton Araújo <cltn.info@gmail.com>
2022-10-16 15:12:59 -0300

    Diferença do produto e a diferença de um inteiro

---
+++ DiferencaProdutoSoma/Program.cs
@@ -0,0 +1,56 @@
+﻿/*
+ * Desafio: Subratindo o produto e a soma de um número inteiro.
+ * 
+ * Dado um número inteiro (n), retorne a diferença entre o produto de seus dígitos e a soma de seus dígitos.
+ * 
+ * Entrada: A entrada consiste em um número inteiro ( n );
+ * 
+ * Saída: A saída consiste na multiplicação do produto dos dígitos ( n ), a soma de seus dígitos e a diferenção entre o produto e a soma, como no exemplo a baixo: 
+ * 
+ * Exemplo 1:                                   
+ * 
+ * Entrada: 234
+ * Saída: 15
+ * 
+ * Explicação:
+ * Produto de dígitos: 2 * 3 * 4 = 24
+ * Soma dos dígitos: 2 + 3 + 4 = 9
+ * Resultado: 24 - 9 = 15
+ * 
+ * Exemplo 2:
+ * 
+ * Entrada: 4421
+ * Saída: 21
+ * 
+ * Explicação: 
+
+    Produto de dígitos = 4 * 4 * 2 * 1 = 32
+
+    Soma dos dígitos = 4 + 4 + 2 + 1 = 11
+
+    Resultado = 32 - 11 = 21
+
+ */
+
+
+int n = int.Parse(Console.ReadLine());
+
+int p = 1;
+int s = 0;
+
+while (n > 0)
+{
+
+    int l = n % 10;
+
+    p = p * l;
+    s = s + l;
+    n = n / 10;
+}
+
+Console.WriteLine(p);
+Console.WriteLine(s);
+
+Console.WriteLine(p - s);
+
+Console.ReadKey();
