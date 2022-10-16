1ad63b63b8835c21dd6087a52e0b0572fa454f8f
Cleiton Araújo <cltn.info@gmail.com>
2022-10-16 16:59:27 -0300

    Par ou impar

---
+++ ParOuImpar/Program.cs
@@ -0,0 +1,14 @@
+﻿
+Console.WriteLine("Digite um número: ");
+int numero = int.Parse(Console.ReadLine());
+
+int resto = numero % 2;
+
+if (resto == 1)
+{
+    Console.WriteLine("O número informado é impar!");
+}
+else
+    Console.WriteLine("O número informado é par!");
+
+Console.ReadKey();
